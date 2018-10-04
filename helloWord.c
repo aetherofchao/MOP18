456084ca3befc2d10a983834968ef593116d4372
aetherofchao <aetherofchao@gmail.com>
2018-10-04 14:53:47 +0000

    I have started to make a List

---
+++ helloWord.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct List
+{
+    int array[5]; //je průměrná velikost listu 
+    int count = 0; // ukazatel posledního zapsaného věci
+    
+    int add(int addition)
+    {
+        // TODO        
+    }
+    
+}
+
+int main(void)
+{
+    // run the juwel
+}
