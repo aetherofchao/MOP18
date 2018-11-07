aebdd709e094290b59276a65e202bc13863ffcf4
aetherofchao <aetherofchao@gmail.com>
2018-11-07 21:06:55 +0000

    uspešne jsem implementoval set

--- P-I-1.cc
+++ P-I-1.cc
@@ -1,125 +1,85 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <bits/stdc++.h>
+#include <set>
+#include <iostream>
 
-using namespace std;
-
-struct List{
-    unsigned int Size;
-    unsigned int *Array; 
-    unsigned int Lengh; // ukazatel posledního zapsaného věci
-    bool IsOdd;
-};
-void add(struct List *node, unsigned int addition);
 int BellFordEsq(unsigned int pocatek);
 int NxtGen();
 void AddToReturn(unsigned int position);
-void deletion(unsigned int a, unsigned int b);
 
 unsigned int n, m, k;
-struct List *Struct;
-unsigned int *Gen, *GenTmp, *predecesor, *ToRtrn, *Tmp;
+std::set<int> *SetsArray;
+bool *OddnesArray;
+unsigned int *Gen, *GenTmp, *pre, *ToRtrn, *Tmp;
 unsigned int countG, countGT, coutTR, from, into;
 
-
-//  >>>>> Maaaain <<<<<
 int main()
 {
-    cin >> n >> m >> k;
-    
-    if (k == m) {cout << 0 << endl; return 0;}
-    
+    std::cin >> n >> m >> k;
     countG = 0; coutTR = 0;
-    Struct = (struct List*)calloc(n +1, sizeof(struct List));
-    for (int i = 0; i <= n; i++) {
-        Struct[i].Size=5*sizeof(unsigned int); 
-        Struct[i].Array = (unsigned int*)calloc(5,sizeof(unsigned int)); 
-        Struct[i].Lengh = 0;
-        Struct[i].IsOdd = false;
-    }
-    Gen = (unsigned int *)calloc(n,sizeof(unsigned int)); 
+    SetsArray = new std::set<int> [n+1];
+    OddnesArray = (bool*)calloc(n+1, sizeof(bool));
+    for (int i = 0; i <= n; i++) {OddnesArray[i] = false;} // iniciuj jako sude
+    Gen = (unsigned int *)calloc(n,sizeof(unsigned int)); // tyto proměné musí být globalní, ale jejich velikost muzeme urcit az po nactení n
     GenTmp = (unsigned int *)calloc(n,sizeof(unsigned int)); 
     ToRtrn = (unsigned int *)calloc(2*n,sizeof(unsigned int)); 
-    predecesor =(unsigned int *)calloc(n+1,sizeof(unsigned int)); 
-    for (int i = 0; i < m; i++) {
-        cin >> from >> into;
-        if(Struct[from].IsOdd) Struct[from].IsOdd = false;  else Struct[from].IsOdd = true;
-        if(Struct[into].IsOdd) Struct[into].IsOdd = false;  else Struct[into].IsOdd = true;
+    pre =(unsigned int *)calloc(n+1,sizeof(unsigned int)); // predesesors
+    for (int i = 0; i < m; i++) { // load
+        std::cin >> from >> into;
+        if(OddnesArray[from]) OddnesArray[from] = false;  else OddnesArray[from] = true; // řeší lichost 
+        if(OddnesArray[into]) OddnesArray[into] = false;  else OddnesArray[into] = true; // řeší lichost 
         if(i >= k){
-            add(&Struct[from],into);  // z 1ho města do druhého 
-            add(&Struct[into],from);  // Z 2ho do 1ho
+            SetsArray[from].insert(into);
+            SetsArray[into].insert(from);} // ulice do k je zakazane pouzívat
+    }
+    for (unsigned int i = 1; i <= n; i++){
+        if ((OddnesArray[i]) && (BellFordEsq(i) == 1)){
+            std::cout << -1 << std::endl; return 0;
         }
-    }
-    for (unsigned int i = 1; i <= n; i++){ if ((Struct[i].IsOdd) && (BellFordEsq(i) == 1)){ cout << -1 << endl; return 0;}}
-    cout << (coutTR/2)<< endl;
-    for (unsigned int i = 0; i < coutTR; i += 2) {cout << ToRtrn[i]<< ' ' << ToRtrn[i + 1] << endl;}
-}
-
-// fc pro přidávání prvků do listu
-void add(struct List *node, unsigned int addition)
-{ 
-    node->Lengh++;
-    if (node->Lengh == node->Size){   
-        node->Size *= 2;   
-        Tmp = (unsigned int*)realloc(node->Array,node->Size);
-        free(node->Array); node->Array = Tmp;
-        
-    }
-    node->Array[node->Lengh] = addition; // add
+    } // proved BellFordEsq a zarozen kontroluj pro nemoznot
+    std::cout << (coutTR/2)<< std::endl; 
+    for (unsigned int i = 0; i < coutTR; i += 2) {
+        std::cout << ToRtrn[i]<< ' ' << ToRtrn[i + 1] << std::endl;} // vypis
 }
 
 int BellFordEsq(unsigned int pocatek)
 {
-    Struct[pocatek].IsOdd = false; // po skoncení nebude licha
-    // první generace se skádá pouze z pocátku
-    Gen[0] = pocatek;  countG = 1;
-    // predecesor musi byt ze zacatku prazdny
-    for (unsigned int i = 0; i <=n; i++){ predecesor[i] = 0;}
-    predecesor[pocatek] = pocatek; // pocatek už byl použit
-    while (true){ // dokud to neudelas nedostanes večeři
-        if (countG == 0)  return 1; // zadna cesta nevede do říma 
-        if (NxtGen() == 1)return 0; // vsehno probehlo spatne
+    OddnesArray[pocatek] = false; // po skoncení nebude licha
+    Gen[0] = pocatek; countG = 1;// první generace se skádá pouze z pocátku
+    for (unsigned int i = 0; i <=n; i++){
+        pre[i] = 0;} // predecesor musi byt ze zacatku prazdny
+    pre[pocatek] = pocatek; // pocatek už byl použit
+    while (true){ // nehrozí možnost nekonečného cyklu
+        if (countG == 0)  return 1; // zadna cesta nevede
+        if (NxtGen() == 1)return 0; // fc NxtGen zbourala cestu a zbavila se tak dvou lichých měst
     } 
 }
 int NxtGen()
 {
-    Tmp = GenTmp;
-    GenTmp = Gen;   countGT = countG;
-    Gen = Tmp;      countG = 0;
+    Tmp = GenTmp; GenTmp = Gen; Gen = Tmp; // otoč generace
+    countGT = countG; countG = 0; // nový musí nahradit stare a ti pak zestarnout
     for (unsigned int tmp = 0; tmp <= countGT; tmp++){
-        unsigned int pocatek = GenTmp[tmp];
-        for (int i = 0; i <= Struct[pocatek].Lengh; i++){
-            unsigned int dalsi = Struct[pocatek].Array[i]; 
-            if (dalsi == 0 || predecesor[dalsi] != 0) continue; // nezajímej se o hodnoty 0 aka. ty které jsme vyřadily 
-            predecesor[dalsi] = pocatek;  //cesta k počátku
-            if (Struct[dalsi].IsOdd){ // dorazili jsme do cile
-                Struct[dalsi].IsOdd = false;
+        for(auto dalsi : SetsArray[GenTmp[tmp]]){
+            if (pre[dalsi] != 0) continue; // nezajímej se o hodnoty 0 aka. ty které jsme vyřadily 
+            pre[dalsi] = GenTmp[tmp];  //cesta k počátku
+            if (OddnesArray[dalsi]){ // dorazili jsme do cile
+                OddnesArray[dalsi] = false;
                 AddToReturn(dalsi); return 1; // musime se ujistit ze zadna cesta není zbouraná dvakrat --> deletion
-            }Gen[countG] = dalsi; countG++;}
+            }
+            Gen[countG] = dalsi; countG++;
+        }
     }return 0;
 }
 
 void AddToReturn(unsigned int position)
 {
-    if (predecesor[position] == position) return;
-    ToRtrn[coutTR] = position;   
-    ToRtrn[coutTR+ 1] = predecesor[position];
+    if (pre[position] == position) return;
+    ToRtrn[coutTR] = position; ToRtrn[coutTR+ 1] = pre[position];
     coutTR += 2;
-    // vymaz cestu 
-    deletion(predecesor[position], position);
-    deletion(position, predecesor[position]);
-    // tato operace je kontroverzní ??????? neměl bych raději ukládat číslo ulice v predesesor[2,] ???????
-    AddToReturn(predecesor[position]);
-}
-
-void deletion(unsigned int a, unsigned int b)
-{
-    for (int i = 0; i < Struct[a].Lengh; i++){
-        if(Struct[a].Array[i] == b) {
-            Struct[a].Array[i] = 0; return;
-        }
-    }
+    SetsArray[pre[position]].erase(position); 
+    SetsArray[position].erase(pre[position]);// vymaz cestu obema smery
+    AddToReturn(pre[position]); // rekurze
 }
 
 
@@ -157,7 +117,7 @@ void deletion(unsigned int a, unsigned int b)
 8 9
 9 10
 10 11
-8 7
+5 6
 
 5 6 1
 1 2
